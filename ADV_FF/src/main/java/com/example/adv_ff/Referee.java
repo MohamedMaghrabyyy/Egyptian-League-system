ebfcace103e93b70715a2e6ab185785a7c1342ee
Mohamed ElMaghraby <125492063+MohamedMaghrabyyy@users.noreply.github.com>
2023-09-22 00:11:27 +0300

    project uploaded

---
+++ ADV_FF/src/main/java/com/example/adv_ff/Referee.java
@@ -0,0 +1,16 @@
+package com.example.adv_ff;
+
+import java.util.ArrayList;
+
+public class Referee extends Person {
+    private ArrayList<Match> listOfMatches;
+
+    public Referee(String name, double weight, double height) {
+        super(name, weight, height);
+    }
+
+    public ArrayList<Match> getListOfMatches() {
+        return listOfMatches;
+    }
+
+}
